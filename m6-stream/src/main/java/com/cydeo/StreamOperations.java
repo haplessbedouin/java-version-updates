5c2f9207d9048911e543afd796b076ef680c1073
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-03-28 14:59:07 -0500

    Streams intermediate operations

---
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -0,0 +1,64 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOperations {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1,2,2,3,4);
+        list.forEach(System.out::println);
+
+        System.out.println("**********************************************************");
+
+        //FILTERING A STREAM
+        list.stream()
+                .filter(i -> i%2==0)
+                .forEach(System.out::println);
+
+        System.out.println("**************************************************************");
+
+        //DISTINCT
+        Stream<Integer> str = list.stream()
+                .filter(i -> i%2 == 0)
+                .distinct();
+        str.forEach(System.out::println);
+
+        System.out.println("*************************************************************");
+
+        //LIMIT
+        list.stream()
+                .filter(i -> i%2 == 0)
+                .limit(1)
+                .forEach(System.out::println);
+
+        System.out.println("*******************************************************************");
+
+        //SKIP
+        list.stream()
+                .filter(i -> i%2 == 0)
+                .skip(1)
+                .forEach(System.out::println);
+
+        System.out.println("*******************************************************************");
+
+        //MAPPING
+        list.stream()
+                .map(number -> number*2)
+                .filter(i -> i%3 == 0)
+                .forEach(System.out::println);
+
+        System.out.println("*****************************************************************");
+
+
+
+
+
+
+
+
+    }
+
+}
